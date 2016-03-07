5bc1e843d9bd3d4d094bf9045530cfa9f36c0a37
Chenye Yin <elizebethyin@gmail.com>
2016-03-07 09:33:37 -0500

    LOL

--- src/GradeAplication.java
+++ src/GradeAplication.java
@@ -9,10 +9,11 @@ public class GradeAplication {
 		String[] name = new String[100], grade = new String[100], gender = new String[100], major = new String[100],
 				origin = new String[100];
 		int[] score = new int[100], genders = new int[2], majors = new int[3], state = new int[3];
-		int counter = 1, totalScore = 0, mean = 0, femScore = 0, meanFem = 0, malScore = 0, meanMal = 0, finScore = 0,
+		int counter = 1, totalScore = 0, femScore = 0, meanFem = 0, malScore = 0, meanMal = 0, finScore = 0,
 				meanFin = 0, mathScore = 0, meanMath = 0, phyScore = 0, meanPhy = 0, mdScore = 0, meanMd = 0,
 				vgScore = 0, meanVg = 0, paScore = 0, meanPa = 0;
-		String yn,current;
+		double mean;
+		String yn, current;
 
 		System.out.println("Enter the student record :");
 		String[] record = sc.nextLine().split("\\s+");
@@ -61,20 +62,19 @@ public class GradeAplication {
 		}
 		totalScore += score[counter - 1];
 		mean = totalScore / counter;
-		counter++;
 		System.out.println("Continue?(y/n)");
 		yn = sc.nextLine();
 		while (yn.toLowerCase().equals("y")) {
-			System.out.println(counter);
+			counter++;
 			System.out.println("Enter the student record :");
 			String[] newRecord = sc.nextLine().split("\\s+");
-			
-			name[counter-1]=newRecord[0]+" "+newRecord[1];
-						grade[counter-1]=newRecord[2];
-						gender[counter-1]=newRecord[3];
-						major[counter-1]=newRecord[4];
-						origin[counter-1]=newRecord[5];
-						score[counter-1]=Integer.valueOf(newRecord[newRecord.length-1]);
+
+			name[counter - 1] = newRecord[0] + " " + newRecord[1];
+			grade[counter - 1] = newRecord[2];
+			gender[counter - 1] = newRecord[3];
+			major[counter - 1] = newRecord[4];
+			origin[counter - 1] = newRecord[5];
+			score[counter - 1] = Integer.valueOf(newRecord[newRecord.length - 1]);
 			if (gender[counter - 1].toLowerCase().equals("m")) {
 				malScore += score[counter - 1];
 				genders[0]++;
@@ -112,28 +112,30 @@ public class GradeAplication {
 				state[2]++;
 				meanPa = paScore / state[2];
 			}
-			
+
 			totalScore += score[counter - 1];
-			mean = totalScore / counter;
-			counter++;
+			mean = totalScore / (counter);
+			
 			System.out.println("Do you want to see current result?(y/n)");
-			current=sc.nextLine();
-			if(current.toLowerCase().equals("y")){
+			current = sc.nextLine();
+			if (current.toLowerCase().equals("y")) {
 				System.out.println("Overall Average: " + mean);
 				System.out.println("Average for Gender: Female=" + meanFem + " Male=" + meanMal);
-				System.out.println("Average for Major: Finance=" + meanFin + " Math=" + meanMath + " Physics=" + meanPhy);
-				System.out.println("Average by State: Maryland=" + meanMd + " Virginia=" + meanVg + " Pensylvania=" + meanPa);
+				System.out
+						.println("Average for Major: Finance=" + meanFin + " Math=" + meanMath + " Physics=" + meanPhy);
+				System.out.println(
+						"Average by State: Maryland=" + meanMd + " Virginia=" + meanVg + " Pensylvania=" + meanPa);
 			}
 			System.out.println("Continue?(y/n)");
 			yn = sc.nextLine();
-			
+
 		}
 		System.out.println("Report Date: " + date.toString());
 		System.out.println("Number of Students: " + counter);
 		System.out.println("Overall Average: " + mean);
 		System.out.println("Average for Gender: Female=" + meanFem + " Male=" + meanMal);
 		System.out.println("Average for Major: Finance=" + meanFin + " Math=" + meanMath + " Physics=" + meanPhy);
-		System.out.println("Average by State: Maryland=" + meanMd + " Virginia=" + meanVg + " Pensylvanis=" + meanPa);
+		System.out.println("Average by State: Maryland=" + meanMd + " Virginia=" + meanVg + " Pensylvania=" + meanPa);
 		sc.close();
 	}
 
